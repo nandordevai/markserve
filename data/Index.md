ff8a6724dfccb6df07c246641b3d2b4da2e128dc
Nandor Devai <mail@nandordevai.net>
2021-04-30 13:04:42 +0200

    add tables and image size keywords

--- data/Index.md
+++ data/Index.md
@@ -6,8 +6,27 @@ Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam praesentium solut
 
 > Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam praesentium soluta doloremque eaque, libero autem placeat quos repudiandae cupiditate ab reprehenderit iure neque suscipit, sunt eum nesciunt! In, eius suscipit!
 
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam praesentium soluta doloremque eaque, libero autem placeat quos repudiandae cupiditate ab reprehenderit iure neque suscipit, sunt eum nesciunt! In, eius suscipit!
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam praesentium soluta doloremque eaque, libero autem placeat quos repudiandae cupiditate ab reprehenderit iure neque suscipit, sunt eum nesciunt! In, eius suscipit!
+- list item 1
+- list item 2
+- list item 3
+    - list item 3.1
+    - list item 3.2
+    - list item 3.3
+        - list item 3.3.1
+        - list item 3.3.2
+        - list item 3.3.3
 
+| table | with    | three columns |
+| ----- | ------- | ------------- |
+| test  | another | final cell    |
+
+![xs](../static/2761183_460s.jpg)
+
+![s](../static/2761183_460s.jpg)
+
+![](../static/2761183_460s.jpg)
+
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam praesentium soluta doloremque eaque, libero autem placeat quos repudiandae cupiditate ab reprehenderit iure neque suscipit, sunt eum nesciunt! In, eius suscipit!
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam praesentium soluta doloremque eaque, libero autem placeat quos repudiandae cupiditate ab reprehenderit iure neque suscipit, sunt eum nesciunt! In, eius suscipit!
 
 [[wiki link]]
--- server.py
+++ server.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 debug = True
 app.config.update(
     DATADIR='data',
-    IMGDIR='static/images',
     DEBUG=debug
 )
 
@@ -18,10 +17,51 @@ def get_filename(page):
     return os.path.join(app.config['DATADIR'], page.replace('_', ' ') + '.md')
 
 
+def add_file_entry(name):
+    if name[-3:] == '.md':
+        return {'name': name[:-3], 'children': None}
+    else:
+        return {'name': name, 'children': []}
+
+
+def get_tree(start=app.config['DATADIR']):
+    # os.walk() output:
+    # /Users/nandi/Projects/Web/markserve/data ['things'] ['Index.md', 'wiki link.md']
+    # /Users/nandi/Projects/Web/markserve/data/things [] ['one thing.md']
+    # convert to:
+    # [
+    #   {
+    #     'name': 'things',
+    #     'children': [
+    #       {
+    #         'name': 'one thing',
+    #         'children': None
+    #       }
+    #     ]
+    #   },
+    #   {
+    #     'name': 'Index',
+    #     'children': None
+    #   },
+    #   {
+    #     'name': 'wiki link',
+    #     'children': None
+    #   }
+    # ]
+    items = []
+    for root, dirs, files in os.walk(start):
+        if len(dirs) > 0:
+            for d in dirs:
+                items.append({'name': d, 'children': get_tree(root)})
+        if len(files) > 0:
+            for f in files:
+                items.append({'name': f, 'children': None})
+    return items
+
+
 def get_pages():
-    return [file[:-3]
-            for file in sorted(os.listdir(app.config['DATADIR']))
-            if file.endswith('.md')]
+    return [add_file_entry(file)
+            for file in sorted(os.listdir(app.config['DATADIR']))]
 
 
 @ app.route('/', methods=['GET'])
@@ -39,7 +79,7 @@ def show_page(page):
     except IOError:
         abort(500)
     html = markdown.markdown(content, extensions=[
-                             WikiLinkExtension(end_url='')])
+                             WikiLinkExtension(end_url=''), 'tables'])
     return render_template('page.html', title=page, content=html, pages=get_pages())
 
 
